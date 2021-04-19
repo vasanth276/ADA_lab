895e9734e77e4bc0d37110dd24b2fb0dde6991e0
vasanth276 <63463535+vasanth276@users.noreply.github.com>
2021-04-19 13:02:45 +0530

    Add files via upload

---
+++ GCD_iterative/GCD_iterative/main.c
@@ -0,0 +1,29 @@
+//
+//  main.c
+//  GCD_iterative
+//
+//  Created by Vasanth Kumar on 19/04/21.
+//
+
+#include<stdio.h>
+int euclid(int a , int b)
+{
+    int q,r;
+
+    while(b>0)
+{
+    q = a/b;
+    r = a-q*b;
+    a = b;
+    b = r;
+}
+return a;
+}
+
+int main()
+{
+    int a = 288;
+    int b = 108;
+    printf("euclid(%d,%d) = %d",a,b,euclid(a,b));
+    return 0;
+}
